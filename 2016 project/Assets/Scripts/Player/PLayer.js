c4c667538656a2402d4c4d6858b71964156250e6
Sepiormon <sepiormon@gmail.com>
2016-01-08 20:50:04 -0700

    Added Notes
    Extra notes on code for easier understanding

--- 2016 project/Assets/Scripts/Player/PLayer.js
+++ 2016 project/Assets/Scripts/Player/PLayer.js
@@ -2,27 +2,27 @@
 
 var MovementSpeed : float = 10;
 var Speedlimit : float = 10;
-var JumpHeight : float = 10;
+var JumpHeight : float = 400;
 
 function Start () {
 
 }
 
 function Update () {
-//movement
-	if(Input.GetKey("a")){
+//movement via acceleration
+	if(Input.GetKey("a")){ //left
 		GetComponent.<Rigidbody2D>().AddForce(new Vector2(-MovementSpeed, 0));
 	}
-	if(Input.GetKey("d")){
+	if(Input.GetKey("d")){ //right
 		GetComponent.<Rigidbody2D>().AddForce(new Vector2(MovementSpeed, 0));
 	}
-	//Speedlimit
+	//Speedlimit via velocity
 	if(GetComponent.<Rigidbody2D>().velocity.x < -Speedlimit){
 		GetComponent.<Rigidbody2D>().velocity.x = -Speedlimit;
 	} else if(GetComponent.<Rigidbody2D>().velocity.x > Speedlimit){
 		GetComponent.<Rigidbody2D>().velocity.x = Speedlimit;
 	}
-	//jump
+	//jump (via massive upwards acceleration)
 	if(Input.GetKeyDown("space")){
 		GetComponent.<Rigidbody2D>().AddForce(new Vector2(0, JumpHeight));
 	}
