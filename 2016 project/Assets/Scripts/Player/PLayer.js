3fd50c2de28e167df7fd099baf34a0508b026f87
Sepiormon <sepiormon@gmail.com>
2016-01-08 20:18:37 -0700

    Moving to Unity5

--- 2016 project/Assets/Scripts/Player/PLayer.js
+++ 2016 project/Assets/Scripts/Player/PLayer.js
@@ -1,9 +1,20 @@
 ﻿#pragma strict
 
+var MovementSpeed : float = 5;
+var Speedlimit : float = 20;
 function Start () {
 
 }
 
 function Update () {
-
+//movement
+	if(Input.GetKey("a")){
+		rigidbody2D.AddForce(new Vector2(-MovementSpeed, 0));
+	}
+	if(Input.GetKey("d")){
+		rigidbody2D.AddForce(new Vector2(MovementSpeed, 0));
+	}
+//Speedlimit
+	if(rigidbody2D.GetVe)
+//end movement
 }
