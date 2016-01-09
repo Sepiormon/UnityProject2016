f2b65235466c6f90fae518c9c8562769a054cc30
Sepiormon <sepiormon@gmail.com>
2016-01-09 16:52:46 -0700

    Added Jumping Mechanics

--- 2016 project/Assets/Scripts/Player/Player.cs
+++ 2016 project/Assets/Scripts/Player/Player.cs
@@ -3,35 +3,55 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	float MovementSpeed = 10;
-	float SpeedLimit = 10;
-	float JumpHeight = 400;
+    float MovementSpeed = 10;
+    float SpeedLimit = 10;
+    float JumpHeight = 400;
+    bool OnGround = false;
 
-	void Start () {
-	
-	}
-	
-	
-	void Update () {
-	//movement via acceleration
-		if(Input.GetKey("a")){
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(-MovementSpeed, 0));
-		}
-		if(Input.GetKey("d")){
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(MovementSpeed, 0));
-		}
-		//Speed limit via velocity
-        var rb = GetComponent<Rigidbody2D>();
+    void Start()
+    {
 
-		if(GetComponent<Rigidbody2D>().velocity.x < -SpeedLimit){
-			rb.velocity = new Vector2(-SpeedLimit,rb.velocity.y);
-		} else if(GetComponent<Rigidbody2D>().velocity.x > SpeedLimit){
+    }
+
+
+    void Update()
+    {
+		var rb = GetComponent<Rigidbody2D>();
+        //movement via acceleration
+        if (Input.GetKey("a")) {
+            rb.AddForce(new Vector2(-MovementSpeed, 0));
+        }
+        if (Input.GetKey("d")) {
+            rb.AddForce(new Vector2(MovementSpeed, 0));
+        }
+        //Speed limit via velocity
+        
+        if (rb.velocity.x < -SpeedLimit) {
+            rb.velocity = new Vector2(-SpeedLimit, rb.velocity.y);
+        } else if (rb.velocity.x > SpeedLimit) {
             rb.velocity = new Vector2(SpeedLimit, rb.velocity.y);
-		}
-		//jump (via massive upwards acceleration)
-		if(Input.GetKeyDown("space")){
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpHeight));
-		}
-	//end movement
-	}
+        }
+        //jump (via massive upwards acceleration)
+        if (Input.GetKeyDown("space") && OnGround) {
+            rb.AddForce(new Vector2(0, JumpHeight));
+        }
+        //end movement
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            OnGround = true; //landing on ground.
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            OnGround = false; //no longer touching ground.
+        }
+    }
+
 }
