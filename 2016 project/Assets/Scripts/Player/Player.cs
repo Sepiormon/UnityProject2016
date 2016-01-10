e884e4ef646b5dcb54ea70df649e89b1013a0aa3
Sepiormon <sepiormon@gmail.com>
2016-01-09 17:18:29 -0700

    Fixed Speed Limit // added jump mechanics (C#)

--- 2016 project/Assets/Scripts/Player/Player.cs
+++ 2016 project/Assets/Scripts/Player/Player.cs
@@ -6,13 +6,16 @@ public class Player : MonoBehaviour {
 	float MovementSpeed = 10;
 	float SpeedLimit = 10;
 	float JumpHeight = 400;
+	bool OnGround = false;
 
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	
-	void Update () {
+	void Update ()
+	{
 	//movement via acceleration
 		if(Input.GetKey("a")){
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(-MovementSpeed, 0));
@@ -23,15 +26,35 @@ public class Player : MonoBehaviour {
 		//Speed limit via velocity
         var rb = GetComponent<Rigidbody2D>();
 
-		if(GetComponent<Rigidbody2D>().velocity.x < -SpeedLimit){
+		if(GetComponent<Rigidbody2D>().velocity.x < -SpeedLimit)
+		{
 			rb.velocity = new Vector2(-SpeedLimit,rb.velocity.y);
-		} else if(GetComponent<Rigidbody2D>().velocity.x > SpeedLimit){
+		}
+		if(GetComponent<Rigidbody2D>().velocity.x > SpeedLimit)
+		{
             rb.velocity = new Vector2(SpeedLimit, rb.velocity.y);
 		}
 		//jump (via massive upwards acceleration)
-		if(Input.GetKeyDown("space")){
+		if(Input.GetKeyDown("space") && OnGround)
+		{
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpHeight));
 		}
 	//end movement
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if(other.gameObject.tag == "Ground")
+		{
+			OnGround = true; //Landing on ground
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D other)
+	{
+		if(other.gameObject.tag == "Ground")
+		{
+			OnGround = false; //Off ground
+		}
 	}
 }
