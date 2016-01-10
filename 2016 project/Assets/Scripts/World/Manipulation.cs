4a50fb10ef4fbdb2999f6ab4c0274d09805cc50b
Sepiormon <sepiormon@gmail.com>
2016-01-10 00:48:25 -0700

    Added light effects and limits to maniuplation
    and background

--- 2016 project/Assets/Scripts/World/Manipulation.cs
+++ 2016 project/Assets/Scripts/World/Manipulation.cs
@@ -5,35 +5,79 @@ public class Manipulation : MonoBehaviour {
 
 	float distance = 10;
 	Material DefMat;
+	bool triggered = false;
+	GameObject Lite;
+	bool lighton = false;
+	public GameObject Player;
 
 	void Start()
-	{
+	{	
+		Debug.Log("why2");
 		DefMat = GetComponent<Renderer>().material;
+		Lite = new GameObject("Manipulatior Light");
+		Light liteComp = Lite.AddComponent<Light>();
+		liteComp.color = Color.blue;
+		liteComp.intensity = 15;
+	}
+
+	void Update()
+	{
+		if(lighton)
+		{
+			Lite.SetActive(true);
+		}
+		else 
+		{
+			Lite.SetActive(false);
+		}
 	}
 
 	void OnMouseOver()
 	{
-		var mat = GetComponent<Renderer>().material;
+		float pdistance = Vector3.Distance(Player.transform.position, gameObject.transform.position);
 		var rb = GetComponent<Rigidbody2D>();
-		if(Input.GetMouseButton(0)){
-			mat.color = Color.blue;
+		if(Input.GetMouseButton(0) && !triggered && pdistance < 10)
+		{
+			Lite.transform.position = gameObject.transform.position;
+			lighton = true;
 			rb.velocity = Vector2.zero;
 			rb.gravityScale = 0;
 			Vector3 mousePosition = Input.mousePosition;
 			mousePosition.z = distance;
 			transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
-		} else {
-			mat = DefMat;
+		} 
+		else 
+		{
 			rb.gravityScale = 1;
+			lighton = false;
 		}
 	}
 
 	void OnMouseExit()
 	{
-		var mat = GetComponent<Renderer>().material;
-		mat = DefMat;
-		GetComponent<Rigidbody2D>().gravityScale = 1;
+		if(lighton)
+		{
+			GetComponent<Rigidbody2D>().gravityScale = 1;
+			lighton = false;
+		}
 	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("yolo");
+		triggered = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		triggered = false;
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		triggered = true;
+	}
+
 }	
 
 
